ba3f1da1e67d63e3cc97a88e573496bac766a34b
Mridul Kalra <mridulkalra700@gmail.com>
2025-09-12 17:07:02 +0530

    improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Signup } from './pages/signup';
+import { Signup } from './pages/Signup';
 import { Signin } from './pages/Signin';
 import { Dashboard } from './pages/Dashboard';
 import { SendMoney } from './pages/SendMoney';
